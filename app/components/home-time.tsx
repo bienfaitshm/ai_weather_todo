d8497d17f9b6fa4aefac1f3eb9551a20ea4b0d06
bienfait shomari <bienfaitshm@gmail.com>
2025-04-05 18:41:09 +0200

    feat(theme): ajouter la gestion des thèmes avec session storage et composants typographiques

--- app/components/home-time.tsx
+++ app/components/home-time.tsx
@@ -1,13 +1,14 @@
 import { useRealTime } from "@/hooks/timer"
 import { formatDateTime, FORMAT_DATE } from "@/lib/date-time"
+import { TypographyH1, TypographyH3 } from "./ui/typography"
 
 
 export const HomeDateTimer: React.FC = () => {
     const time = useRealTime()
     return (
         <div className="flex flex-col items-center">
-            <h1 className="text-7xl font-bold capitalize">{formatDateTime(time, FORMAT_DATE.HOME_TIME)}</h1>
-            <h3 className="text-md capitalize">{formatDateTime(time, FORMAT_DATE.HOME_DATE)}</h3>
+            <TypographyH1 className="lg:text-7xl capitalize">{formatDateTime(time, FORMAT_DATE.HOME_TIME)}</TypographyH1>
+            <TypographyH3 className="text-md font-normal capitalize">{formatDateTime(time, FORMAT_DATE.HOME_DATE)}</TypographyH3>
         </div>
     )
 }
