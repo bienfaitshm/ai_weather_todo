bbb8b676c9ed65ead5849a168dd68b282e872018
bienfait shomari <bienfaitshm@gmail.com>
2025-04-05 18:58:17 +0200

    fix(mode-toggle): supprimer le variant "ghost" du bouton dans le composant de basculement de thème

--- app/components/home-time.tsx
+++ app/components/home-time.tsx
@@ -7,8 +7,8 @@ export const HomeDateTimer: React.FC = () => {
     const time = useRealTime()
     return (
         <div className="flex flex-col items-center">
-            <TypographyH1 className="lg:text-7xl capitalize">{formatDateTime(time, FORMAT_DATE.HOME_TIME)}</TypographyH1>
-            <TypographyH3 className="text-md font-normal capitalize">{formatDateTime(time, FORMAT_DATE.HOME_DATE)}</TypographyH3>
+            <TypographyH1 className="lg:text-7xl capitalize dark:text-white">{formatDateTime(time, FORMAT_DATE.HOME_TIME)}</TypographyH1>
+            <TypographyH3 className="text-md font-normal capitalize dark:text-neutral-300">{formatDateTime(time, FORMAT_DATE.HOME_DATE)}</TypographyH3>
         </div>
     )
 }
