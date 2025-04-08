872f679278d433c9fbc591b644bbae2c6589f30d
bienfait shomari <bienfaitshm@gmail.com>
2025-04-08 05:50:44 +0200

    feat(task-schema): ajouter le schéma de validation des tâches avec Zod

---
+++ app/lib/schemas/index.ts
@@ -0,0 +1,20 @@
+import {z} from "zod"
+
+export const TaskSchema = z.object({
+    // The title of the task, must be a non-empty string
+    title: z.string().nonempty(),
+
+    // A detailed description of the task, must be a optionnal string
+    description: z.string().optional(),
+
+    // A boolean indicating whether the task is completed or not
+    completed: z.boolean(),
+
+    // An optional string representing the color associated with the task
+    color: z.string().optional(),
+
+    // An optional string representing the due date of the task
+    dueDate: z.string().optional(),
+});
+
+export type Task = z.infer<typeof TaskSchema>;
