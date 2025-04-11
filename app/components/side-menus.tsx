2ee42660bb9d02474179735fe1dac182b23327fe
bienfait shomari <bienfaitshm@gmail.com>
2025-04-11 20:02:23 +0200

    feat(side-menus): ajouter le composant SideMenus pour la gestion des actions de confirmation

---
+++ app/components/side-menus.tsx
@@ -0,0 +1,30 @@
+import {
+    Sheet,
+    SheetContent,
+    SheetDescription,
+    SheetHeader,
+    SheetTitle,
+    SheetTrigger,
+} from "@/components/ui/sheet"
+
+interface SideMenusProps {
+    btnOpen?: React.ReactNode;
+}
+
+export const SideMenus: React.FC<SideMenusProps> = ({ btnOpen }) => {
+    return (
+        <Sheet>
+            <SheetTrigger asChild>{btnOpen}</SheetTrigger>
+            <SheetContent>
+                <SheetHeader>
+                    <SheetTitle>Are you absolutely sure?</SheetTitle>
+                    <SheetDescription>
+                        This action cannot be undone. This will permanently delete your account
+                        and remove your data from our servers.
+                    </SheetDescription>
+                </SheetHeader>
+            </SheetContent>
+        </Sheet>
+
+    )
+}
