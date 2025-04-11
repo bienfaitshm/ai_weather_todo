b9a3b7fe7913fe5c9a728076d3a9ce469e6b462d
bienfait shomari <bienfaitshm@gmail.com>
2025-04-11 21:27:59 +0200

    feat(side-menus): ajouter des liens de navigation pour chaque menu et traduire le texte en français

--- app/components/side-menus.tsx
+++ app/components/side-menus.tsx
@@ -8,6 +8,41 @@ import {
     SheetTrigger,
 } from "@/components/ui/sheet"
 import { TypographyP } from "./ui/typography";
+import { HomeIcon, ListTodoIcon, CloudMoonIcon, MapPinHouseIcon, InfoIcon, type LucideProps } from "lucide-react";
+import { Link } from "@remix-run/react";
+
+type Menu = {
+    icon: React.ForwardRefExoticComponent<Omit<LucideProps, "ref"> & React.RefAttributes<SVGSVGElement>>,
+    name: string,
+    href: string
+}
+const menus: Menu[] = [
+    {
+        icon: HomeIcon,
+        name: "Accueil",
+        href: "/",
+    },
+    {
+        icon: ListTodoIcon,
+        name: "Mes tâches",
+        href: "/tasks",
+    },
+    {
+        icon: CloudMoonIcon,
+        name: "Météos et Prévisions",
+        href: "/weather",
+    },
+    {
+        icon: MapPinHouseIcon,
+        name: "Informations",
+        href: "/info",
+    },
+    {
+        icon: InfoIcon,
+        name: "À propos",
+        href: "/about",
+    },
+]
 
 interface SideMenusProps {
     btnOpen?: React.ReactNode;
@@ -21,23 +56,34 @@ export const SideMenus: React.FC<SideMenusProps> = ({ btnOpen }) => {
                 <SheetHeader>
                     <SheetTitle>Menus</SheetTitle>
                     <SheetDescription>
-                        This is a simple application to manage your tasks and check the weather.
+                        Ceci est une application simple pour gérer vos tâches et vérifier la météo.
                     </SheetDescription>
 
                 </SheetHeader>
-                <div className="flex flex-1 flex-col gap-4 mt-4">
-                    <div>
+                <div className="flex flex-1 mt-4 w-full">
+                    <div className="space-y-2 w-full">
+                        {
+                            menus.map((menu) => {
+                                const Icon = menu.icon;
+                                return (
+                                    <Link to={menu.href} key={menu.name} className="flex items-center gap-2 p-2 rounded hover:bg-gray-100 cursor-pointer w-full">
+                                        <Icon className="w-4 h-4" />
+                                        <TypographyP>{menu.name}</TypographyP>
+                                    </Link>
+                                )
+                            })
+                        }
 
                     </div>
                 </div>
                 <SheetFooter className=" sm:flex-col sm:justify-center sm:space-x-2">
-                    <div className="flex items-center gap-2 text-sm">
+                    <div className="flex gap-2 text-sm">
                         <div className="w-3 h-3 rounded-full bg-green-600 animate-pulse"></div>
-                        <TypographyP><b>Lubumbashi</b> d'après votre position actuelle</TypographyP>
+                        <TypographyP><b>Lubumbashi</b> <span className="text-xs">selon votre position actuelle</span></TypographyP>
                     </div>
-                    <div className="flex flex-col items-center justify-center gap-2 mt-4">
-                        <p className="text-sm text-muted-foreground">© {new Date().getFullYear()} Ai Weather Todo</p>
-                        <p className="text-sm text-muted-foreground">All rights reserved.</p>
+                    <div className="flex flex-col items-center justify-center gap-2 mt-4 sm:text-xs text-sm text-muted-foreground">
+                        <p>© {new Date().getFullYear()} Ai Weather Todo</p>
+                        <p>Tous droits réservés.</p>
                     </div>
                 </SheetFooter>
             </SheetContent>
