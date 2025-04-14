28ae5f2ea7658a670d4b0fd147b65a44991e875e
bienfait shomari <bienfaitshm@gmail.com>
2025-04-14 12:35:40 +0200

    feat: Ajout de la fonctionnalité de minuterie avec formatage de l'heure et intégration du plugin Netlify

--- app/components/home-time.tsx
+++ app/components/home-time.tsx
@@ -1,13 +1,20 @@
 import { useRealTime } from "@/hooks/timer"
-
+import { format } from "date-fns"
+import { TypographyH3 } from "./ui/typography"
 
 export const HomeDateTimer = () => {
     const date = useRealTime()
 
     return (
         <div className="flex flex-col items-center justify-center text-center">
-            <h1 className="text-4xl font-bold text-white">{date.toLocaleDateString()}</h1>
-            <p className="text-2xl text-gray-200">{date.toLocaleTimeString()}</p>
+            <h1 className="text-4xl font-bold">{date.toLocaleDateString()}</h1>
+            <div className="grid grid-cols-3 gap-4 mt-4">
+                <TypographyH3 className="text-4xl">{format(date, "HH")}</TypographyH3>
+                <TypographyH3 className="text-4xl">{format(date, ": mm")}</TypographyH3>
+                <div className="flex items-end">
+                    <TypographyH3>{format(date, "ss")}</TypographyH3>
+                </div>
+            </div>
         </div>
     )
 }
