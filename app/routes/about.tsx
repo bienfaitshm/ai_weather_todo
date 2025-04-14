7f0e71052fe846204feb701c8a0988bb4da727b3
bienfait shomari <bienfaitshm@gmail.com>
2025-04-14 15:06:42 +0200

    feat(About): Mise à jour de la description et amélioration de la mise en page de la page À propos

--- app/routes/about.tsx
+++ app/routes/about.tsx
@@ -1,15 +1,11 @@
 import { Link } from "@remix-run/react";
 import type { MetaFunction } from "@remix-run/node";
-
-import { TypographyP } from "@/components/ui/typography";
-import { SideMenus } from "@/components/side-menus";
-import { Button } from "@/components/ui/button";
-import { PanelLeft } from "lucide-react";
+import { TypographyH1, TypographyP } from "@/components/ui/typography";
 
 export const meta: MetaFunction = () => {
     return [
         { title: "Ai Weather Todo | Apropos" },
-        { name: "description", content: "Cette application combine une liste de tâches des mises à jour météo et une intelligence artificielle pour offrir une expérience utilisateur fluide et intelligente." },
+        { name: "description", content: "Cette application combine une liste de tâches des mises à jour météo et une intelligence artificielle pour offrir une expérience utilisateur fluide et intelligente, développée à titre d'apprentissage des nouvelles technologies." },
     ];
 };
 
@@ -17,27 +13,29 @@ export const meta: MetaFunction = () => {
 
 export default function Page() {
     return (
-        <div className="flex h-screen continue mx-auto max-w-screen-md items-center justify-center">
-            <div className="text-center">
-                <div className="flex items-center gap-4">
-                    <SideMenus btnOpen={
-                        <Button>
-                            <PanelLeft className="h-4 w-4" />
-                            <span className="sr-only">Ouvrir le menu</span>
-                        </Button>
-                    } />
-                    <h1 className="text-4xl font-bold text-gray-800">Bienvenue sur Ai Weather Todo</h1>
-                </div>
+        <div className="flex mt-10 md:mt-28 continue mx-auto max-w-screen-md items-center justify-center">
+            <div className="text-center space-y-4">
+                <TypographyH1>Bienvenue sur Ai Weather Todo</TypographyH1>
                 <p className="mt-4 text-lg text-gray-600">
-                    Ceci est une application simple pour gérer vos tâches et consulter la météo.
+                    Ce projet est une application simple pour gérer vos tâches et consulter la météo,
+                    développée à titre d'apprentissage des nouvelles technologies.
                 </p>
-                <TypographyP>
-                    Cette application combine une liste de tâches,
-                    des mises à jour météo et une intelligence artificielle
-                    pour offrir une expérience utilisateur fluide et intelligente.
-                    <Link to="https://github.com/bienfaitshm" className="text-blue-600 underline hover:text-blue-800">Bienfait Shomari</Link>,
-                    elle vise à rendre votre planification quotidienne plus intelligente et efficace.
-                </TypographyP>
+
+                <div className="text-justify">
+                    <TypographyP>
+                        Cette application open source combine une liste de tâches,
+                        des mises à jour météo et une intelligence artificielle
+                        pour offrir une expérience utilisateur fluide et intelligente.
+                    </TypographyP>
+                    <TypographyP>
+                        Développée par <Link to="https://github.com/bienfaitshm" className="text-blue-600 underline hover:text-blue-800 mx-2">Bienfait Shomari</Link>,
+                        elle vise à rendre votre planification quotidienne plus intelligente et efficace.
+                    </TypographyP>
+                    <TypographyP>
+                        Les contributions sont les bienvenues sur le dépôt GitHub du projet :{" "}
+                        <Link to="https://github.com/bienfaitshm/ai_weather_todo" className="text-blue-600 underline hover:text-blue-800">ai_weather_todo</Link>.
+                    </TypographyP>
+                </div>
             </div>
         </div>
     );
