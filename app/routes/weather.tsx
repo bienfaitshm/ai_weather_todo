b3bb17e9328cd4955bafa7373e21f458c2dd1d90
bienfait shomari <bienfaitshm@gmail.com>
2025-04-14 13:35:12 +0200

    refactor(Weather): Amélioration de la mise en page et suppression de composants inutilisés

--- app/routes/weather.tsx
+++ app/routes/weather.tsx
@@ -1,12 +1,9 @@
-import { Button } from "@/components/ui/button"
-import { TypographyH2, TypographyH3, TypographyH4, TypographySmall } from "@/components/ui/typography"
-import { CloudSnowIcon, CloudMoon, ArrowLeftIcon } from "lucide-react"
-import { useNavigate } from "@remix-run/react";
-import { TaskSheet } from "@/components/task-sheet";
+import { TypographyH1, TypographyH3, TypographyH4, TypographySmall } from "@/components/ui/typography"
+import { CloudSnowIcon, CloudMoon } from "lucide-react"
 
 const WeatherCardPrevesion: React.FC = () => {
     return (
-        <div className="flex flex-col items-center gap-5">
+        <div className="flex flex-col items-center gap-5  rounded-md border p-2 shadow-sm md:shadow-none md:border-none">
             <TypographyH4 className="text-md">27 Sun</TypographyH4>
             <CloudSnowIcon className="size-14" />
             <div className="flex flex-col items-center gap-2">
@@ -22,12 +19,13 @@ const WeatherCardPrevesion: React.FC = () => {
 
 const WeatherMainCardPrevesion: React.FC = () => {
     return (
-        <div className="space-y-4">
+        <div className="flex flex-row items-center justify-between md:flex-col md:items-start">
             <div className="">
-                <TypographyH2>35°</TypographyH2>
+                <TypographyH4>Aujourd'hui</TypographyH4>
+                <TypographyH1>35°</TypographyH1>
                 <div className="flex flex-col">
-                    <TypographySmall className="text-xs">Shower</TypographySmall>
-                    <TypographySmall className="text-xs">Chance of Rain 90%</TypographySmall>
+                    <TypographySmall className="text-sm">Shower</TypographySmall>
+                    <TypographySmall className="text-sm">Chance of Rain 90%</TypographySmall>
                 </div>
             </div>
             <CloudMoon className="size-28" />
@@ -35,37 +33,20 @@ const WeatherMainCardPrevesion: React.FC = () => {
     )
 }
 
-const ButtonGoBack: React.FC = () => {
-    const navigate = useNavigate()
-    return (
-        <Button size="icon" variant="outline" className="rounded-full" onClick={() => navigate(-1)}>
-            <ArrowLeftIcon />
-        </Button>
-    )
-}
-
 export default function PrevisionPage() {
     return (
-        <div className="container flex h-screen items-center mx-auto max-w-screen-lg">
+        <div className="p-4 container flex items-center mx-auto max-w-screen-lg mt-10 md:mt-28">
             <div className="w-full space-y-6">
-                <div className="flex items-center justify-between">
-                    <div className="flex items-center gap-4">
-                        <ButtonGoBack />
-                        <TypographyH3>Lubumbashi-CD</TypographyH3>
-                    </div>
-                    <div className="flex items-center gap-4">
-                        <TaskSheet />
-                    </div>
+                <div className="flex items-center justify-between gap-4">
+                    <TypographyH3>Lubumbashi-CD</TypographyH3>
                 </div>
-                <div className="grid grid-cols-7 gap-8 w-full">
-                    <div className="col-span-2">
+                <div className="grid grid-cols-2 gap-4 md:grid-cols-7 md:gap-8 w-full">
+                    <div className="col-span-2 mb-20">
                         <WeatherMainCardPrevesion />
                     </div>
-                    <WeatherCardPrevesion />
-                    <WeatherCardPrevesion />
-                    <WeatherCardPrevesion />
-                    <WeatherCardPrevesion />
-                    <WeatherCardPrevesion />
+                    {Array.from({ length: 5 }).map((_, index) => (
+                        <WeatherCardPrevesion key={index} />
+                    ))}
                 </div>
             </div>
         </div>
