798b5924f0e2c5c7e138dd0be2d02a0c2b45e6b5
bienfait shomari <bienfaitshm@gmail.com>
2025-04-15 15:41:25 +0200

    refactor(app-sidebar): add dynamique city name

--- app/components/app-sidebar.tsx
+++ app/components/app-sidebar.tsx
@@ -14,18 +14,11 @@ import { Link, useLoaderData } from "@remix-run/react"
 
 import { menus } from "@/constants/menus"
 import { TypographyH3, TypographyP } from "./ui/typography"
-// import { getPosition } from "@/.server/geo"
-import { LoaderFunctionArgs } from "@remix-run/node"
-
-// export async function loader({
-//     request,
-// }: LoaderFunctionArgs) {
-//     return await getPosition(request)
-// }
+import type { loader } from "@/root"
 
 
 export const AppSidebar = () => {
-    const data = useLoaderData();
+    const { geo: { city } } = useLoaderData<typeof loader>();
     return (
         <Sidebar>
             <SidebarContent className="flex flex-col">
@@ -33,7 +26,6 @@ export const AppSidebar = () => {
                     <TypographyH3 className="text-lg font-bold">Menu</TypographyH3>
                     <TypographyP className="text-xs text-muted-foreground">
                         Ceci est une application simple pour gérer vos tâches et vérifier la météo.
-                        {JSON.stringify(data)}
                     </TypographyP>
                 </SidebarHeader>
                 <SidebarGroup className="flex-1 mt-5">
@@ -57,13 +49,12 @@ export const AppSidebar = () => {
                     <div className="flex items-center gap-2 mt-4 sm:text-xs text-sm text-muted-foreground flex-wrap">
                         <div className="size-2 rounded-full bg-green-600 animate-pulse flex-shrink-0"></div>
                         <TypographyP className="flex-1">
-                            <b>Lubumbashi</b> <span className="text-xs">(position actuelle)</span>
+                            <b>{city}</b> <span className="text-xs">(position actuelle)</span>
                         </TypographyP>
                     </div>
                     <div className="flex flex-col items-center justify-center sm:text-xs text-sm text-muted-foreground">
                         <TypographyP>© {new Date().getFullYear()} - Tous droits réservés</TypographyP>
                         <TypographyP>Ai Weather Todo</TypographyP>
-                        {/* <TypographyP>Version {process.env.APP_VERSION || "1.0.0"}</TypographyP> */}
                     </div>
                 </SidebarFooter>
             </SidebarContent>
