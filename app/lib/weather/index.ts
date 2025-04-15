84e7e019781596c904a3daf65256e931d54ab020
bienfait shomari <bienfaitshm@gmail.com>
2025-04-15 15:38:17 +0200

    refactor: remove unused code

--- app/lib/weather/index.ts
+++ app/lib/weather/index.ts
@@ -1,20 +1,19 @@
 import axios from "axios"
 
-// const API_URL = "http://api.weatherapi.com/v1/forecast.json?key=d59471d9bb934e5197785400250504&q=Lubumbashi&days=7&aqi=no&alerts=no"
+const baseURL = "http://api.weatherapi.com/v1/"
+const axiosClient = axios.create({baseURL });
 
-const axiosClient = axios.create({
-    baseURL: "http://api.weatherapi.com/v1/"
-})
+
 export type Region = {
     city: string
 }
 
-export async function getWeatherForecast(region: Region) {
+export async function getWeatherForecast(region: Region, days:number = 7) {
     return await axiosClient.get("forecast.json", {
         params: {
             key: "d59471d9bb934e5197785400250504",
             q: region.city,
-            days: 7,
+            days,
             aqi: "no",
             alerts: "no"
         }
