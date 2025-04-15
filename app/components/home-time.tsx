1e7617241a06b604a863efe29ab6907d91276bdb
bienfait shomari <bienfaitshm@gmail.com>
2025-04-15 22:59:26 +0200

    fix(home-time): ajuster les classes de style pour le composant HomeDateTimer

--- app/components/home-time.tsx
+++ app/components/home-time.tsx
@@ -11,8 +11,8 @@ export const HomeDateTimer = () => {
         <div className="flex flex-col items-center justify-center text-center">
             <div className="grid grid-cols-5 gap-4 mt-4 px-5">
                 <TypographyH1 className="lg:text-8xl col-span-4">{formatDateTime(date, FORMAT_DATE.HOME_TIME)}</TypographyH1>
-                <div className="flex items-end mb-2 max-w-10 ">
-                    <TypographyH3>{format(date, FORMAT_DATE.SECOND)}</TypographyH3>
+                <div className="flex items-end md-1 md:mb-2 max-w-10 ">
+                    <TypographyH3 className="text-xs md:text-lg">{format(date, FORMAT_DATE.SECOND)}</TypographyH3>
                 </div>
             </div>
             <div className="text-2xl mt-4">{formatDateTime(date, FORMAT_DATE.HOME_DATE)}</div>
